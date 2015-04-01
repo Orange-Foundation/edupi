9a8417c39f95041d4df71b1a6e14113cad89e5de
Yuancheng Peng <herve.yuancheng@gmail.com>
2015-04-01 15:46:51 +0200

    add basic template view

--- cntapp/static/cntapp/js/views.js
+++ cntapp/static/cntapp/js/views.js
@@ -15,6 +15,21 @@
         return o;
     };
 
+    var TemplateView = Backbone.View.extend({
+        templateName: '',
+        initialize: function () {
+            this.template = _.template($(this.templateName).html());
+        },
+        render: function () {
+            var context = this.getContext(),
+                html = this.template(context);
+            this.$el.html(html);
+        },
+        getContext: function () {
+            return {};
+        }
+    });
+
     var RootDirectoriesView = Backbone.View.extend({
         el: "#content",
         templateName: "#directories-template",
