67f792cac18887b52d620d57ad3120dc4a524cd4
Yuancheng Peng <herve.yuancheng@gmail.com>
2015-04-14 18:36:02 +0200

    add search document by name or description on server side

--- edupi/pagination.py
+++ edupi/pagination.py
@@ -1,13 +1,24 @@
 from rest_framework.pagination import LimitOffsetPagination, CursorPagination
 from rest_framework.response import Response
 from rest_framework.compat import OrderedDict
+from django.db.models import Q, CharField
 
 
 class SimpleLimitOffsetPagination(LimitOffsetPagination):
     sort_query_param = 'sort'
     order_query_param = 'order'
+    search_query_param = 'search'
 
     def paginate_queryset(self, queryset, request, view=None):
+        search_term = request.query_params.get(self.search_query_param, None)
+        if search_term is not None:
+            fields = [f for f in queryset.model._meta.fields if isinstance(f, CharField)]
+            queries = [Q(**{f.name + '__contains': search_term}) for f in fields]
+            qs = Q()
+            for query in queries:
+                qs = qs | query
+            queryset = queryset.filter(qs)
+
         sort_attr = request.query_params.get(self.sort_query_param, None)
         if sort_attr is not None:
             order = request.query_params.get(self.order_query_param, None)
