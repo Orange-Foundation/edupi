76ec1fc1fb2a4400d65fdc91837687c9ac1c0036
Yuancheng Peng <herve.yuancheng@gmail.com>
2015-04-29 18:24:12 +0200

    re-organaze project & start dev of final user UI

--- cntapp/views/views.py
+++ cntapp/views/views.py
@@ -13,20 +13,6 @@ def index(request):
     return render(request, 'cntapp/index.html')
 
 
-def root(request):
-    return directory(request)
-
-
-def directory(request, dir_id=None):
-    if dir_id is None:
-        dirs = [d for d in (Directory.objects.all()) if d.get_parents().count() == 0]
-    else:
-        d = get_object_or_404(Directory, pk=dir_id)
-        dirs = d.get_sub_dirs()
-
-    return render(request, 'cntapp/dir_list.html', {'dirs': dirs})
-
-
 class DocumentViewSet(viewsets.ModelViewSet):
     """
     This viewset list `documents`, and provides `create`, `retrieve`,
