9927683f868df590eb2c292bcea5e9626d5e6a88
Yuancheng <yuancheng.peng@telecom-bretagne.eu>
2015-05-03 21:22:10 +0200

    refac: move bootstrap table control to JS

--- cntapp/static/cntapp/custom/src/views/documents_table.js
+++ cntapp/static/cntapp/custom/src/views/documents_table.js
@@ -1,22 +1,44 @@
 define([
-    'jquery',
     'underscore',
     'backbone',
-    'views/template',
-    'text!templates/documents_table.html',
 
     'bootstrap_table'
-], function ($, _, Backbone, TemplateView, documentsTableTemplate) {
-    var DocumentsTableView = TemplateView.extend({
-        initialize: function () {
-            this.template = _.template(documentsTableTemplate);
-        },
+], function (_, Backbone) {
+
+    var DocumentsTableView = Backbone.View.extend({
         render: function () {
-            TemplateView.prototype.render.apply(this);
-            // convert Document models to native JSON objects and pass to table
-            $('#table').bootstrapTable();
+            this.$el.html("<table id='table'></table>");
+
+            this.$('#table').bootstrapTable({
+                height: 519,
+                url: '/api/documents/',
+                pagination: 'true',
+                showRefresh: 'true',
+                showColumns: 'true',
+                showToggle: 'true',
+                search: 'true',
+                sidePagination: 'server',
+
+                columns: [{
+                    field: 'name',
+                    title: 'Name'
+                }, {
+                    field: 'description',
+                    title: 'Description'
+                }, {
+                    field: 'action',
+                    title: 'Action',
+                    formatter: function () {
+                        return [
+                            '<a class="remove" href="javascript:void(0)" title="Remove">',
+                            '<i class="glyphicon glyphicon-remove"></i>',
+                            '</a>'
+                        ].join('');
+                    }
+                }]
+            });
             // adjust the header
-            $('#table').bootstrapTable('resetView');
+            this.$('#table').bootstrapTable('resetView');
             return this;
         }
     });
