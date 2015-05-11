8a8caf8dc5cc6cf8a43fb27231f7d9f82d2cec8a
Yuancheng Peng <herve.yuancheng@gmail.com>
2015-05-11 19:45:31 +0200

    give up generating thumbnail when timeout(30 secs)

--- cntapp/serializers.py
+++ cntapp/serializers.py
@@ -1,16 +1,19 @@
 from datetime import datetime
-import os
 import tempfile
 import logging
+import os
+import threading
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 from wand.image import Image
 from rest_framework import serializers
 
+from .models import Directory, Document
+
 
 logger = logging.getLogger(__name__)
 
-from .models import Directory, Document
+THUMBNAIL_CREATE_TIMEOUT = 30  # second
 
 
 class DirectorySerializer(serializers.ModelSerializer):
@@ -41,6 +44,18 @@ class DocumentSerializer(serializers.ModelSerializer):
         else:
             validated_data['type'] = Document.TYPE_OTHERS
 
+    @staticmethod
+    def create_pdf_thumbnail(validated_data):
+        file_name = None
+        # use page[0] as thumbnail
+        with Image(filename=validated_data['file'].temporary_file_path() + '[0]') as img:
+            file_name = tempfile.mktemp(suffix='.png')
+            img.save(filename=file_name)  # save to /tmp
+        if file_name is not None:
+            file_path = os.path.join('/tmp', file_name)
+            with open(file_name, 'rb') as f:
+                validated_data['thumbnail'] = SimpleUploadedFile(file_name, f.read())
+
     def create(self, validated_data):
         logger.info('copying files...')
         self.fill_document_type(validated_data)
@@ -59,15 +74,12 @@ class DocumentSerializer(serializers.ModelSerializer):
                 validated_data['thumbnail'] = SimpleUploadedFile(uploaded_file.name, f.read())
 
         elif content_type in ['application/pdf']:
-            file_name = None
-            # use page[0] as thumbnail
-            with Image(filename=validated_data['file'].temporary_file_path() + '[0]') as img:
-                file_name = tempfile.mktemp(suffix='.png')
-                img.save(filename=file_name)  # save to /tmp
-            if file_name is not None:
-                file_path = os.path.join('/tmp', file_name)
-                with open(file_name, 'rb') as f:
-                    validated_data['thumbnail'] = SimpleUploadedFile(file_name, f.read())
+            t = threading.Thread(name='create-pdf-thumbnail',
+                                 target=self.create_pdf_thumbnail, args=(validated_data,))
+            t.start()
+            t.join(timeout=THUMBNAIL_CREATE_TIMEOUT)
+            if 'thumbnail' not in validated_data:
+                logger.warn('thumbnail is not generated for "%s"' % validated_data['name'])
 
         logger.info('%d secs elapsed for generating the file...' % (datetime.now() - start).seconds)
 
