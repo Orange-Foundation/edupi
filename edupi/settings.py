27caad5a3d77c2104efef04dfd0aa26050f192c0
Yuancheng Peng <herve.yuancheng@gmail.com>
2015-07-15 11:31:22 +0200

    correct nginx log file prefix

--- edupi/settings.py
+++ edupi/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK_EXTENSIONS = {
 }
 
 NGINX_LOG_DIR = '/var/log/nginx/'
-NGINX_MEDIA_ACCESS_LOG_PREFIX = 'edupi_nginx_media_access_'
+NGINX_MEDIA_ACCESS_LOG_PREFIX = 'edupi_media_access'
 STATS_DIR = os.path.join(BASE_DIR, '../stats/')
 
 LOGGING = {
