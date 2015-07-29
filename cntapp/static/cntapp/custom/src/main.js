d9bdc37827cc360d618060ab8866a58572ca42da
Yuancheng Peng <herve.yuancheng@gmail.com>
2015-07-29 10:22:29 +0200

    v1.4.1

--- cntapp/static/cntapp/custom/src/main.js
+++ cntapp/static/cntapp/custom/src/main.js
@@ -1,7 +1,7 @@
 require.config({
     // for development purpose, ensure the newest version js at each time
     //urlArgs: "bust=" + (new Date()).getTime(),
-    urlArgs: "bust=v1.4.0",
+    urlArgs: "bust=v1.4.1",
 
     paths: {
         'jquery': '/static/jquery/dist/jquery',
